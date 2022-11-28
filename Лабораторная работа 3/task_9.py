cf4b8fe4a34ebdb9e475cfedbc869da8dc4c292d
Yulia <118075465+12345Stol@users.noreply.github.com>
2022-11-28 18:38:58 +0500

    Update task_9.py

--- Лабораторная работа 3/task_9.py
+++ Лабораторная работа 3/task_9.py
@@ -5,9 +5,8 @@ increase = 0.03  # рост цен
 
 money_capital = 0  # количество денег, чтобы прожить 10 месяцев
 
-while months > 0:
-    delta = spend - salary  # Определяем долг
-    spend *= 1 + increase  # Траты с учетом роста цен
+for i in range(months): # Перебираем месяца 
+    delta = spend - salary # Долги 
+    spend *= 1 + increase # Траты с учетом роста цен
     money_capital += delta
-    months -= 1  # Перебираем месяцы
 print(round(money_capital))
