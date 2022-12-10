c06fd61ff396dfd8c80011fb090042509e9d0f83
Yulia <118075465+12345Stol@users.noreply.github.com>
2022-12-10 15:44:29 +0500

    Update task_3.py

--- Лабораторная работа 5/task_3.py
+++ Лабораторная работа 5/task_3.py
@@ -1,14 +1,18 @@
-import random
+from random import randint
 
 
 def get_unique_list_numbers() -> list[int]:
-    list_number = list(range(-10, 11))  # Создадим список значений от -10 до 10 включительно
-    unique_number = random.sample(list_number, 15)  # Отберем 15 случайных уникальных значений
-    return unique_number
+    start = -10
+    stop = 10
+    symbols = 15
+    list_number = []
+    while len(list_number) < symbols:
+        number = randint(start, stop)
+        if number not in list_number:
+            list_number.append(number)
+    return list_number
 
 
 list_unique_numbers = get_unique_list_numbers()
 print(list_unique_numbers)
 print(len(list_unique_numbers) == len(set(list_unique_numbers)))
-
-
