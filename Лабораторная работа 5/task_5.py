571734c984126f4974b64b261443ef4a5bebe10e
Yulia <118075465+12345Stol@users.noreply.github.com>
2022-12-10 15:45:17 +0500

    Update task_5.py

--- Лабораторная работа 5/task_5.py
+++ Лабораторная работа 5/task_5.py
@@ -1,10 +1,9 @@
 from random import sample
-import string
+from string import ascii_uppercase, ascii_lowercase, digits
 
 
-def get_random_password() -> str:
-    n = 8  # Допустимое кол-во символов в пароле
-    symbol = string.ascii_uppercase + string.ascii_lowercase + string.digits  # Обозначим допустимые символы
+def get_random_password(n=8) -> str:
+    symbol = ascii_uppercase + ascii_lowercase + digits  # Обозначим допустимые символы
     random_password = "".join(sample(symbol, n))  # Генерируем случайную выборку символов для пароля
     return random_password
 
